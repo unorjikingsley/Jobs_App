657fc170f7865b930992cdcf7505b11c7d0a1b8e
unorji kingsley <unorjikingsley@gmail.com>
2023-10-30 07:49:09 +0100

    Code to connect to MongoDB

--- db/connect.js
+++ db/connect.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = (url) => {
   return mongoose.connect(url)
+  
   // return mongoose.connect(url, {
   //   useNewUrlParser: true,
   //   useCreateIndex: true,
