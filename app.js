10ee263a2534a7df3a110b077784ef2747d0b81d
unorji kingsley <unorjikingsley@gmail.com>
2023-11-01 17:10:51 +0100

    Login functionalities

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 //connect to mongoDB
 const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication')
 
 // routes
 const authRouter = require('./routes/auth');
@@ -20,7 +21,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobsRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
