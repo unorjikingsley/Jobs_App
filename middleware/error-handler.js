cbb0a3c29597c79eea56318e091bb99298e99c80
unorji kingsley <unorjikingsley@gmail.com>
2023-11-02 18:21:22 +0100

    Job models

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -9,6 +9,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // if (err instanceof CustomAPIError) {
   //   return res.status(err.statusCode).json({ msg: err.message })
   // }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
 
   if (err.name === 'ValidationError') {
     customError.msg = Object.values(err.errors)
@@ -16,12 +17,14 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       .join(',')
     customError.statusCode = 400
   }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`
     customError.statusCode = 400
   }
+  
   if (err.name === 'CastError') {
     customError.msg = `No item found with id : ${err.value}`
     customError.statusCode = 404
